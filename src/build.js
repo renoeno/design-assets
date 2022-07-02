7d5ff42de03fef06d143107c5fc0db9c2b4e12a0
renoeno <reno.less@gmail.com>
2022-07-02 15:15:07 -0300

    First commit

---
+++ src/build.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const assetModuleFolder = ["icons"];
+const { svgToJS } = require("./utils/svgToJS");
+
+assetModuleFolder.forEach((asset) => {
+  const options = {
+    inputDir: `src/assets/${asset}`,
+    outputDir: `dist/assets/${asset}`,
+  };
+
+  svgToJS(options);
+});
